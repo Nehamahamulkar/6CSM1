55767d66c4ce94b195e1a8418b497996a2dcc09b
Aditi <Aditi@LAPTOP-NPL6PJ59>
2022-12-30 10:19:45 +0530

    this is my first commit

---
+++ FirstProject/src/com/Qspider/Demo.java
@@ -0,0 +1,10 @@
+package com.Qspider;
+
+public class Demo extends Test {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ FirstProject/src/com/Qspider/Test.java
@@ -0,0 +1,8 @@
+package com.Qspider;
+
+public class Test {
+
+	public static void m1() {
+		System.out.println("neha is student");
+	}
+}
